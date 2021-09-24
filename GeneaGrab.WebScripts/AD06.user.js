43d3cb4a1b32d8be8e692eb461c29a9bcb480e51
EvanG <evan_g@orange.fr>
2021-09-24 21:38:56 +0200

    Formatting

--- GeneaGrab.WebScripts/AD06.user.js
+++ GeneaGrab.WebScripts/AD06.user.js
@@ -13,63 +13,62 @@
 //     Recherche
 // *****************
 if (window.location.href.startsWith("http://www.basesdocumentaires-cg06.fr/archives/rechercheEC")) {
-  document.querySelectorAll("a.image:link").forEach(function (l) {
-	$link = $(l);
-	$args = $link.attr("href").replace("javascript:lancerVisuV2('", "").replace("');", "").split("','");
-	if ($args.length === 6) {
-	  var url = "http://www.basesdocumentaires-cg06.fr/archives/ImageZoomViewerEC.php?HR=1&IDDOC=" + $args[1] + "&COMMUNE=" + $args[2] + "&PAROISSE=" + $args[3] + "&TYPEACTE=" + $args[4] + "&DATE=" + $args[5];
-	  
-	  $link.text("Visualiseur")
-		.attr("href", url)
-		.attr("target", "_blank");
-	  
-	  $link.clone()
-		.text("Geneagrab")
-		.attr("href", "geneagrab:registry?url="+ encodeURIComponent(url))
-		.removeAttr("target")
-		.css("margin-left", "1em")
-		.appendTo($link.parent());
-	}
-  });
+	document.querySelectorAll("a.image:link").forEach(function (l) {
+		$link = $(l);
+		$args = $link.attr("href").replace("javascript:lancerVisuV2('", "").replace("');", "").split("','");
+		if ($args.length === 6) {
+			var url = "http://www.basesdocumentaires-cg06.fr/archives/ImageZoomViewerEC.php?HR=1&IDDOC=" + $args[1] + "&COMMUNE=" + $args[2] + "&PAROISSE=" + $args[3] + "&TYPEACTE=" + $args[4] + "&DATE=" + $args[5];
+
+			$link.text("Visualiseur").attr("href", url).attr("target", "_blank");
+
+			$link
+				.clone()
+				.text("Geneagrab")
+				.attr("href", "geneagrab:registry?url=" + encodeURIComponent(url))
+				.removeAttr("target")
+				.css("margin-left", "1em")
+				.appendTo($link.parent());
+		}
+	});
 }
 
 // ****************
 //      Viewer
 // ****************
 if (window.location.href.startsWith("http://www.basesdocumentaires-cg06.fr/archives/ImageZoomViewer")) {
-  $pageDiv = $("td #textDiv.textDiv");
-  $closeBtn = $('a.lien[href="javascript:self.close()"]');
-  $buttons = $closeBtn.parent();
+	$pageDiv = $("td #textDiv.textDiv");
+	$closeBtn = $('a.lien[href="javascript:self.close()"]');
+	$buttons = $closeBtn.parent();
 
-  $('<a class="lien" href="">Copier les arguments de la page<input readonly type="text" style="display: none;" /></a>')
-	.appendTo($buttons)
-	.click(function (event) {
-	  event.preventDefault();
-	  $input = $(this).find("input");
+	$('<a class="lien" href="">Copier les arguments de la page<input readonly type="text" style="display: none;" /></a>')
+		.appendTo($buttons)
+		.click(function (event) {
+			event.preventDefault();
+			$input = $(this).find("input");
 
-	  //Arguments
-	  $input.val(window.location.href + "&page=" + $pageDiv.text());
+			//Arguments
+			$input.val(window.location.href + "&page=" + $pageDiv.text());
 
-	  //Copie dans le presse-papier
-	  $input.show();
-	  $input.focus();
-	  $input.select();
-	  document.execCommand("copy");
-	  $input.hide();
+			//Copie dans le presse-papier
+			$input.show();
+			$input.focus();
+			$input.select();
+			document.execCommand("copy");
+			$input.hide();
+		});
+
+	function updateLink() {
+		$openIn.attr("href", "geneagrab:registry?url=" + encodeURIComponent(window.location.href + "&page=" + $pageDiv.text()));
+	}
+	$openIn = $('<a class="lien" href="geneagrab:registry?url=' + encodeURIComponent(window.location.href) + '">Ouvrir dans GeneaGrab</a>')
+		.appendTo($buttons)
+		.hover(updateLink)
+		.mousemove(updateLink)
+		.click(updateLink);
+
+	$buttons.children().each(function () {
+		$(this).css("display", "block");
 	});
-
-  function updateLink() {
-	$openIn.attr("href", "geneagrab:registry?url=" + encodeURIComponent(window.location.href + "&page=" + $pageDiv.text()));
-  }
-  $openIn = $('<a class="lien" href="geneagrab:registry?url=' + encodeURIComponent(window.location.href) + '">Ouvrir dans GeneaGrab</a>')
-	.appendTo($buttons)
-	.hover(updateLink)
-	.mousemove(updateLink)
-	.click(updateLink);
-
-  $buttons.children().each(function () {
-	$(this).css("display", "block");
-  });
-  $buttons.children("span").hide();
-  $closeBtn.hide();
+	$buttons.children("span").hide();
+	$closeBtn.hide();
 }
