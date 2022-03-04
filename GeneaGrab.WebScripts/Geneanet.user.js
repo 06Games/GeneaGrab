04d0a291d2ee040cfc46288fa0c202249ab6e71f
EvanG <evan_g@orange.fr>
2022-03-04 23:05:29 +0100

    Geneanet Extension: Support for the new website

--- GeneaGrab.WebScripts/Geneanet.user.js
+++ GeneaGrab.WebScripts/Geneanet.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Geneanet
 // @icon         https://github.com/06Games/GeneaGrab/raw/v1/GeneaGrab/Assets/Logo/Icon.png
-// @version      1.1.1
+// @version      1.2
 // @grant        unsafeWindow
 // @match        https://www.geneanet.org/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
@@ -11,30 +11,13 @@
 // ==/UserScript==
 /*global waitForKeyElements*/
 
-if (location.pathname.startsWith("/archives/registres/view")) {
-	function marqueursLink() {
-		var i = 0;
-		$("#viewer-marqueurs-liste .voir > a").each(function () {
-			$(this).attr("href", $(this).attr("href").replace("?idcollection=", "").replace("&page=", "/").replace("&idmarqueur=", "?idmarqueur="));
-			i++;
-		});
-		console.log(i.toString() + " marqueurs updated");
-	}
-
-	$(document).ready(marqueursLink);
-	waitForKeyElements("#viewer-marqueurs-liste", function (jNode) {
-		marqueursLink();
-	});
-
-	$(".contribuer").hide();
-	$("#viewer-relier")
-		.removeClass("open-popup-liaison")
-		.attr("href", "geneagrab:registry?url=" + location.href)
+if (location.pathname.startsWith("/registres/view")) {
+	$("#treelink > button")
+		.removeAttr("data-popup-id")
+		.click(function () { window.location = "geneagrab:registry?url=" + encodeURIComponent(location.href); })
 		.attr("title", "Ouvrir dans GeneaGrab");
 } else if (location.pathname.startsWith("/fonds")) {
-	$(".ligne-resultat")
-		.filter(function () {
+	$(".ligne-resultat").filter(function () {
 			return $(this).data("type-fonds") === "annuaire_archives";
-		})
-		.hide();
+		}).hide();
 }
