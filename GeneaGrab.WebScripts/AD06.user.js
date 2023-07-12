80f1dfb7a82782001f2e1f02b120020400844ba2
EvanG <evan_g@orange.fr>
2023-07-12 22:14:55 +0200

    feat(AD06): Update of the userscript

--- GeneaGrab.WebScripts/AD06.user.js
+++ GeneaGrab.WebScripts/AD06.user.js
@@ -1,136 +1,32 @@
 // ==UserScript==
 // @name         AD06
-// @icon         https://github.com/06Games/GeneaGrab/raw/v1/GeneaGrab/Assets/Logo/Icon.png
-// @version      1.1.5
+// @icon         https://github.com/06Games/GeneaGrab/raw/v2/GeneaGrab/Assets/Logo/Icon.png
+// @version      2.0.0
 // @grant        none
-// @include      http://www.basesdocumentaires-cg06.fr/archives/*
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @include      https://archives06.fr/ark:/*
 // @updateURL    https://github.com/06Games/GeneaGrab/raw/v2/GeneaGrab.WebScripts/AD06.user.js
 // @downloadURL  https://github.com/06Games/GeneaGrab/raw/v2/GeneaGrab.WebScripts/AD06.user.js
 // ==/UserScript==
 
-// ****************
-//      Viewer
-// ****************
-if (window.location.href.startsWith("http://www.basesdocumentaires-cg06.fr/archives/ImageZoomViewer")) {
-	$pageDiv = $("td #textDiv.textDiv");
-	$closeBtn = $('a.lien[href="javascript:self.close()"]');
-	$buttons = $closeBtn.parent();
+window.addEventListener("load", function() {
+	if(!document.getElementById("monocle")) return;
 
-	$('<a class="lien" href="">Copier les arguments de la page<input readonly type="text" style="display: none;" /></a>')
-		.appendTo($buttons)
-		.click(function (event) {
-			event.preventDefault();
-			$input = $(this).find("input");
+	let help = document.getElementById("react-tabs-10");
+	let openInGeneagrab = help.cloneNode(false);
+	help.after(openInGeneagrab);
 
-			//Arguments
-			$input.val(window.location.href + "&page=" + $pageDiv.text());
+	openInGeneagrab.id = "react-tabs-geneagrab";
+	openInGeneagrab.setAttribute("title", "Ouvrir dans GeneaGrab")
+	openInGeneagrab.innerHTML = "<img class=\"monocle-Icon\" src=\"https://github.com/06Games/GeneaGrab/raw/v2/GeneaGrab/Assets/Logo/Icon.png\" style=\"filter: brightness(10000%);\">";
+	openInGeneagrab.addEventListener("click", function(e) {
+		 e.preventDefault();
 
-			//Copie dans le presse-papier
-			$input.show();
-			$input.focus();
-			$input.select();
-			document.execCommand("copy");
-			$input.hide();
-		});
+		 const regex = /\/ark:\/(?<something>.*?)\/(?<id>.*?)\/(?<tag>.*?)\/(?<seq>\d*)/;
+		 let matches = regex.exec(window.location).groups;
 
-	function updateLink() {
-		$openIn.attr("href", "geneagrab:registry?url=" + encodeURIComponent(window.location.href + "&page=" + $pageDiv.text()));
-	}
-	$openIn = $('<a class="lien" href="geneagrab:registry?url=' + encodeURIComponent(window.location.href) + '">Ouvrir dans GeneaGrab</a>')
-		.appendTo($buttons)
-		.hover(updateLink)
-		.mousemove(updateLink)
-		.click(updateLink);
-
-	$buttons.children().each(function () {
-		$(this).css("display", "block");
+		 let page = document.querySelector(".monocle-PageNav input.rea11y-NumberInput-value").value;
+		 let url = "https://archives06.fr/ark:/" + matches["something"] + "/" + matches["id"] + "/" + matches["tag"] + "/" + matches["seq"] + "/" + page;
+		 window.location = "geneagrab:registry?url=" + encodeURIComponent(url);
 	});
-	$buttons.children("span").hide();
-	$closeBtn.hide();
-}
 
-// *****************
-//    Etat-Civil
-// *****************
-if (window.location.href.startsWith("http://www.basesdocumentaires-cg06.fr/archives/rechercheEC")) {
-	document.querySelectorAll("a.image:link").forEach(function (l) {
-		$link = $(l);
-		$args = $link.attr("href").replace("javascript:lancerVisuV2('", "").replace("');", "").split("','");
-		if ($args.length === 6) {
-			var url = "http://www.basesdocumentaires-cg06.fr/archives/ImageZoomViewerEC.php?HR=1&IDDOC=" + $args[1] + "&COMMUNE=" + $args[2] + "&PAROISSE=" + $args[3] + "&TYPEACTE=" + $args[4] + "&DATE=" + $args[5];
-
-			$link.text("Visualiseur")
-				.attr("href", url)
-				.attr("target", "_blank");
-
-			$link.clone()
-				.text("Geneagrab")
-				.attr("href", "geneagrab:registry?url="+ encodeURIComponent(url))
-				.removeAttr("target")
-				.css("margin-left", "1em")
-				.appendTo($link.parent());
-		}
-	});
-}
-
-// *****************
-//     Cadastre
-// *****************
-if (window.location.href.startsWith("http://www.basesdocumentaires-cg06.fr/archives/rechercheCAD2")) {
-	document.querySelectorAll("a.image:link").forEach(function (l) {
-		if(document.location.search.includes("choix=CAD")){
-			$link = $(l);
-			$args = $link.attr("href").replace("javascript:lancerVisuCAD('", "").replace("');", "").split("','");
-			if ($args.length === 8) {
-				var url = "http://www.basesdocumentaires-cg06.fr/archives/ImageZoomViewerCAD.php"
-					+ "?e=" + $args[1]
-					+ "&c=" + $args[2]
-					+ "&l=" + $args[3]
-					+ "&t=" + $args[4]
-					+ "&cote=" + $args[5]
-					+ "&a=" + $args[6]
-					+ "&che=" + $args[7]
-					+ "&ana=undefined";
-
-				$link.text("Visualiseur")
-					.attr("href", url)
-					.attr("target", "_blank");
-
-				$link.clone()
-					.text("Geneagrab")
-					.attr("href", "geneagrab:registry?url="+ encodeURIComponent(url))
-					.removeAttr("target")
-					.css("margin-left", "1em")
-					.appendTo($link.parent());
-			}
-		}
-		else {
-			$link = $(l);
-			$args = $link.attr("href").replace("javascript:lancerVisuV2MAT('", "").replace("');", "").split("','");
-			if ($args.length === 9) {
-				var url = "http://www.basesdocumentaires-cg06.fr/archives/ImageZoomViewerMAT_ETS.php"
-					+ "?IDDOC=" + $args[1]
-					+ "&COMMUNE=" + $args[2]
-					+ "&COMPLEMENTLIEUX=" + $args[3]
-					+ "&COTE=" + $args[4]
-					+ "&NATURE=" + $args[5]
-					+ "&DATE=" + $args[6]
-					+ "&CHOIX=" + $args[7]
-					+ "&CODECOM=" + $args[8];
-				if(document.location.search.includes("choix=MAT")) url += "&FOLIO=" + $("#" + $args[0]).children('td').eq(3).text();
-
-				$link.text("Visualiseur")
-					.attr("href", url)
-					.attr("target", "_blank");
-
-				$link.clone()
-					.text("Geneagrab")
-					.attr("href", "geneagrab:registry?url="+ encodeURIComponent(url))
-					.removeAttr("target")
-					.css("margin-left", "1em")
-					.appendTo($link.parent());
-			}
-		}
-	});
-}
+}, false);
